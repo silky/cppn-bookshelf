c608c246a40c27844fae9c46482a08414a77af69
silky <noonsilk+-noonsilk@gmail.com>
2020-03-19 11:22:59 +1100

    use csv instead of folder

--- compute-cppns.py
+++ compute-cppns.py
@@ -4,26 +4,30 @@ import os
 import click
 import subprocess
 from utils import ensure_dir_exists
+import pandas as pd
 
 @click.command()
 
-@click.option("--img_path",
-                default="images",
-                help="Folder to load base images from.",
+@click.option("--csv_path",
+                default="out.csv",
+                help="CSV file to read data from.",
                 required=True)
 
 @click.option("--cppn_img_path",
                 default="cppn_images",
                 help="Folder to save cppn images into.",
                 required=True)
-def main (img_path=None, cppn_img_path=None):
-    imgs = os.listdir(img_path)
-    imgs = [ i for i in imgs if i.endswith(".jpg") ]
+def main (csv_path=None, cppn_img_path=None):
+
+    data = pd.read_csv(csv_path)
+    imgs   = data["img"].tolist()
+    titles = data["title"].tolist()
 
     ensure_dir_exists(cppn_img_path)
 
-    for img in imgs:
-        print(f"Processing {img}")
+    for title, img in zip(titles, imgs):
+        filename = os.path.basename(img)
+        print(f"Processing {title}, image = {img}")
         proc = subprocess.Popen(
                 [ "cppn"
                 , "new"
@@ -31,9 +35,9 @@ def main (img_path=None, cppn_img_path=None):
                 , "--z_dim",        "20"
                 , "--activations", "tanh,softplus,tanh,tanh,tanh,softplus,tanh,tanh,tanh,tanh"
                 , "--colours",      "3"
-                , "--out",          f"{cppn_img_path}/{img}"
+                , "--out",          f"{cppn_img_path}/{filename}"
                 , "match"
-                , "--image",        f"{img_path}/{img}"
+                , "--image",        f"{img}"
                 , "--steps",        "2000"
                 , "--log_every",    "2000" # Only log once.
                 ])
